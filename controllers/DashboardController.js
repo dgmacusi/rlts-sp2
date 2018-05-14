f0029f6296fb743501a207f8cfcad5b77a21ea78
Perfeli Adrian Clemente <dgmacusi@gmail.com>
2018-05-14 22:55:31 +0800

    Fixed top statistics on dashboard

--- controllers/DashboardController.js
+++ controllers/DashboardController.js
@@ -3,6 +3,7 @@ var Dashboard = require(`${__dirname}/../models/Dashboard`);
 module.exports = {
 	getHomePage : function (req, res, next) {
 		Dashboard.getTopStatistics(function (err, statistics) {
+			console.log(statistics)
 			res.render('dashboard', { statistics : statistics })
 		})
 	}
--- models/Dashboard.js
+++ models/Dashboard.js
@@ -3,67 +3,49 @@ var mysqlConnection = require(`${__dirname}/../controllers/ConfigurationControll
 module.exports = {
 	getTopStatistics : function (cb) {
 
-		var studentQuery = 'SELECT COUNT(userId) FROM user WHERE type="student";'
-		var teacherQuery = 'SELECT COUNT(userId) FROM user WHERE type="teacher";'
-		var administratorQuery = 'SELECT COUNT(userId) FROM user WHERE type="administrator";'
-		var nonteachingstaffQuery = 'SELECT COUNT(userId) FROM user WHERE type="nonteachingstaff";'
-		var classroomQuery = 'SELECT COUNT(locationId) FROM user WHERE type="classroom";'
-		var facilityQuery = 'SELECT COUNT(locationId) FROM user WHERE type="facility";'
+		var studentQuery = 'SELECT COUNT(userId) AS count FROM user WHERE type="student";'
+		var teacherQuery = 'SELECT COUNT(userId) AS count FROM user WHERE type="teacher";'
+		var administratorQuery = 'SELECT COUNT(userId) AS count FROM user WHERE type="administrator";'
+		var nonteachingstaffQuery = 'SELECT COUNT(userId) AS count FROM user WHERE type="nonteachingstaff";'
+		var classroomQuery = 'SELECT COUNT(locationId) AS count FROM location WHERE type="classroom";'
+		var facilityQuery = 'SELECT COUNT(locationId) AS count FROM location WHERE type="facility";'
 
-		var studentSize = ''
-		var adminSize = ''
-		var teacherSize = ''
-		var nonSize = ''
-		var facilitySize = ''
-		var classroomSize = ''
+		var studentSize = 0
+		var adminSize = 0
+		var teacherSize = 0
+		var nonSize = 0
+		var facilitySize = 0
+		var classroomSize = 0
 
 		mysqlConnection.query(studentQuery, function (err, students) {
-			if (students) {
-				studentSize = students.length;
-			} else {
-				studentSize = 0;
+			if (students[0] != null && students[0] != undefined) {
+				studentSize = students[0].count;
 			}
-			console.log('Students: ' + studentSize)
 
 			mysqlConnection.query(teacherQuery, function (err, teachers) {
-				if (teachers) {
-					teacherSize = teachers.length;
-				} else {
-					teacherSize = 0;
+				if (teachers[0] != null && teachers[0] != undefined) {
+					teacherSize = teachers[0].count;
 				}
-				console.log('Teachers: ' + teacherSize)
 
 				mysqlConnection.query(administratorQuery, function (err, admins) {
-					if (admins) {
-						adminSize = admins.length;
-					} else {
-						adminSize = 0;
+					if (admins[0] != null && admins[0] != undefined) {
+						adminSize = admins[0].count;
 					}
-					console.log('Admins: ' + adminSize)
 
 					mysqlConnection.query(nonteachingstaffQuery, function (err, nons) {
-						if (nons) {
-							nonSize = nons.length;
-						} else {
-							nonSize = 0;
+						if (nons[0] != null && nons[0] != undefined) {
+							nonSize = nons[0].count;
 						}
-						console.log('Non: ' + nonSize)
 
 						mysqlConnection.query(classroomQuery, function (err, classrooms) {
-							if (classrooms) {
-								classroomSize = classrooms.length;
-							} else {
-								classroomSize = 0;
+							if (classrooms[0] != null && classrooms[0] != undefined) {
+								classroomSize = classrooms[0].count;
 							}
-							console.log('Classrooms: ' + classroomSize)
 
 							mysqlConnection.query(facilityQuery, function (err, facilities) {
-								if (facilities) {
-									facilitySize = facilities.length;
-								} else {
-									facilitySize = 0;
+								if (facilities[0] != null && facilities[0] != undefined) {
+									facilitySize = facilities[0].count;
 								}
-								console.log('Facilities: ' + facilitySize)
 
 								var statistics = {
 									studentSize : studentSize, 
@@ -75,7 +57,6 @@ module.exports = {
 									locationSize : classroomSize + facilitySize, 
 									userSize : studentSize + teacherSize + adminSize + nonSize
 								}
-								console.log(statistics)
 
 								return cb(null, statistics)
 
